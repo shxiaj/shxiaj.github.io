a43fd9c6cbc800d45d19463dc400513c32ddacd4
ShXiaJ <shxiaj@users.noreply.github.com>
2026-07-05 01:40:18 +0800

    chore(theme): 移除 PostList 组件，简化自定义主题

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,6 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
-import PostList from './PostList.vue'
 
 export default {
-  ...Theme,
-  enhanceApp({ app }) {
-    app.component('PostList', PostList)
-  },
+  ...Theme
 }
